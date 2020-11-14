b220b4e277ead110471914d5fb9964eb40621a17
JoseD42 <josedanielbecerra42@gmail.com>
2020-11-13 19:13:29 -0700

    Solución mongoose y node

--- schema/Schema.js
+++ schema/Schema.js
@@ -1,12 +1,7 @@
 import graphql from 'graphql';
 
-import RootQueryType from './RootQueryType.js';
-import MutationType from './MutationType.js';
-
 const {GraphQLSchema} = graphql;
 
-
 export default new GraphQLSchema({
-    query: RootQueryType,
-    mutation: MutationType
+
 });
